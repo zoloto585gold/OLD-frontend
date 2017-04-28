19efe169d068e7e01938c4364c69fe972f6cecdf
Max Salnikov <liss38@yandex.ru>
2017-04-28 19:06:51 +0300

    basket with jsrender, basket.html

---
+++ production/js/inc/forms.js
@@ -0,0 +1,34 @@
+// ==============
+// == forms.js ==
+// ==============
+;$(function () {
+	$('.form').on('focusin', function (event) {
+		var $_this = $(event.target);
+		if($_this.hasClass('form__field-input')) $_this.parent().addClass('focus');
+	});
+	$('.form').on('focusout', function (event) {
+		var $_this = $(event.target);
+		if($_this.hasClass('form__field-input')) $_this.parent().removeClass('focus');
+	});
+
+
+	// кастомизированное текстовое поле input type="text" (form-textline.less)
+	$('.form-textline').on('focusin', function (event) {
+		$(this).addClass('form-textline--active');
+	});
+	$('.form-textline').on('focusout', function (event) {
+		$(this).removeClass('form-textline--active');
+	});
+
+
+	// кастомизированное поле для сообщений  textarea (form-textbox.less)
+	$('.form-textbox').on('focusin', function (event) {
+		$(this).addClass('form-textbox--active');
+	});
+	$('.form-textbox').on('focusout', function (event) {
+		$(this).removeClass('form-textbox--active');
+	});
+});
+// 
+// 
+// 
