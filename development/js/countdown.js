dafeef56a14eab201ad3cb785e3505ca7cd6e02a
Max Salnikov <liss38@yandex.ru>
2017-05-16 14:35:03 +0300

    счетчик

--- development/js/countdown.js
+++ development/js/countdown.js
@@ -2,175 +2,152 @@
 // = Обратный счетчик в Header =
 // =======================
 // Create Countdown
-  var Countdown = {
+var Countdown = {
+	// Backbone-like structure
+	$el: $('.js-countdown-header'),
 
-    // Backbone-like structure
-    $el: $('.js-countdown-header'),
+	// Params
+	countdown_interval: null,
+	total_seconds: 0,
 
-    // Params
-    countdown_interval: null,
-    total_seconds: 0,
+	// Initialize the countdown
+	init: function() {
+		// текущее время
+		var timeCurrent = new Date();
+		// установаленный дедлайн
+		var timeDeadline = new Date(this.$el.data('date'));
 
-    // Initialize the countdown
-    init: function() {
-      var Today           = new Date();
-      var Todayday        = Today.getDate();
-      var Todayhours      = Today.getHours();
-      var Todayminutes    = Today.getMinutes();
-      var Todayseconds    = Today.getSeconds();
+		var diff = timeDeadline - timeCurrent,
+			milliseconds,
+			seconds,
+			minutes,
+			hours,
+			days;
 
-      var DeadLine        = new Date(this.$el.data('date'));
-      var DeadLineday     = DeadLine.getDate();
-      var DeadLinehours   = DeadLine.getUTCHours();
-      var DeadLineminutes = DeadLine.getMinutes();
-      var DeadLineseconds = DeadLine.getSeconds();
+		diff = (diff - (milliseconds = diff % 1000 )) / 1000;
+		diff = (diff - (seconds = diff % 60)) / 60;
+		diff = (diff - (minutes = diff % 60)) / 60;
+		days = (diff - (hours = diff % 24)) / 24;
 
-      // DOM
-      this.$ = {
-        days:     this.$el.find('.bloc-time.days .figure'),
-        hours:    this.$el.find('.bloc-time.hours .figure'),
-        minutes:  this.$el.find('.bloc-time.min .figure'),
-        seconds:  this.$el.find('.bloc-time.sec .figure')
-      };
+		// DOM
+		this.$ = {
+			hours: this.$el.find('.bloc-time.hours .figure'),
+			minutes: this.$el.find('.bloc-time.min .figure'),
+			seconds: this.$el.find('.bloc-time.sec .figure')
+		};
 
-      // Init countdown values
-      this.values = {
-        days:     DeadLineday - Todayday,
-        hours:    DeadLinehours - Todayhours,
-        minutes:  DeadLineminutes - Todayminutes,
-        seconds:  DeadLineseconds - Todayseconds
-      };
+		// Initialize total seconds
+		// Initialize total seconds
+		this.total_seconds = (timeDeadline-timeCurrent)/1000;
 
-      // Initialize total seconds
-      // this.total_seconds = ((this.values.days * 24) + (this.values.hours * 60 * 60 + (this.values.minutes * 60))) + this.values.seconds;
-      this.total_seconds = this.values.seconds + (this.values.minutes * 60) + (this.values.hours * 60 * 60) + (this.values.days * 24 * 60 * 60);
+		this.values = {
+			hours:    hours,
+			minutes:  minutes,
+			seconds:  seconds
+		};
 
-      // Animate countdown to the end
-      this.count();
-    },
+		// console.log(this.total_seconds);
+		// console.log(this.values);
 
-    count: function() {
+		// Animate countdown to the end
+		this.count();
+	},
 
-      var that = this,
-        $day_1  = this.$.days.eq(0),
-        $day_2  = this.$.days.eq(1),
-        $hour_1 = this.$.hours.eq(0),
-        $hour_2 = this.$.hours.eq(1),
-        $min_1  = this.$.minutes.eq(0),
-        $min_2  = this.$.minutes.eq(1),
-        $sec_1  = this.$.seconds.eq(0),
-        $sec_2  = this.$.seconds.eq(1);
+	count: function() {
+		var that    = this,
+			$hour_1 = this.$.hours.eq(0),
+			$hour_2 = this.$.hours.eq(1),
+			$min_1  = this.$.minutes.eq(0),
+			$min_2  = this.$.minutes.eq(1),
+			$sec_1  = this.$.seconds.eq(0),
+			$sec_2  = this.$.seconds.eq(1);
 
-      this.countdown_interval = setInterval(function() {
+		this.countdown_interval = setInterval(function() {
 
-        if (that.total_seconds > 0) {
-          --that.values.seconds;
+		if(that.total_seconds > 0) {
+			--that.values.seconds;
 
-          if( that.total_seconds > 90000){
-              that.$.days.parent().show();
-              that.$.minutes.parent().hide();
-          } else {
-              that.$.minutes.parent().show();
-              that.$.days.parent().hide();
-          }
+			if(that.values.minutes >= 0 && that.values.seconds < 0) {
+				that.values.seconds = 59;
+				--that.values.minutes;
+			}
 
-          if (that.values.minutes >= 0 && that.values.seconds <= 0) {
-            that.values.seconds = 59;
-            --that.values.minutes;
-          } // end if
+			if(that.values.hours >= 0 && that.values.minutes < 0) {
+				that.values.minutes = 59;
+				--that.values.hours;
+			}
 
-          if (that.values.hours >= 0 && that.values.minutes <= 0) {
-            that.values.minutes = 59;
-            --that.values.hours;
-          } // end if
+			// Update DOM values
+			// Hours
+			that.checkHour(that.values.hours, $hour_1, $hour_2);
 
-          if (that.values.days >= 0 && that.values.hours <= 0) {
+			// Minutes
+			that.checkHour(that.values.minutes, $min_1, $min_2);
 
-            that.values.hours = 24;
-            --that.values.days;
+			// Seconds
+			that.checkHour(that.values.seconds, $sec_1, $sec_2);
 
-            // that.$minutes.parent().hide();
-          } // end if
+			--that.total_seconds;
+		}
+		else {
+			that.$el.parent().css({'display' : 'none'});
+		}
+		}, 1000);
+	},
 
-          // Update DOM values
-          // Days
-          that.checkDay(that.values.days, $day_1, $day_2);
+	animateFigure: function($el, value) {
+		var that = this,
+			$top = $el.find('.top'),
+			$bottom = $el.find('.bottom'),
+			$back_top = $el.find('.top-back'),
+			$back_bottom = $el.find('.bottom-back');
 
-          // Hours
-          that.checkDay(that.values.hours, $hour_1, $hour_2);
+		// Before we begin, change the back value
+		$back_top.find('span').html(value);
 
-          // Minutes
-          that.checkDay(that.values.minutes, $min_1, $min_2);
+		// Also change the back bottom value
+		$back_bottom.find('span').html(value);
 
-          // Seconds
-          that.checkDay(that.values.seconds, $sec_1, $sec_2);
+		// Then animate
+		TweenMax.to($top, 0.8, {
+			rotationX: '-180deg',
+			transformPerspective: 300,
+			ease: Quart.easeOut,
+			onComplete: function() {
+				$top.html(value);
+				$bottom.html(value);
+				TweenMax.set($top, {
+					rotationX: 0
+				});
+			}
+		});
 
-          --that.total_seconds;
-        } // end if(that.total_seconds > 0)
-        else {
-          clearInterval(that.countdown_interval);
-        }
-      }, 1000);
-    },
+		TweenMax.to($back_top, 0.8, {
+			rotationX: 0,
+			transformPerspective: 300,
+			ease: Quart.easeOut,
+			clearProps: 'all'
+		});
+	},
 
-    animateFigure: function($el, value) {
+	checkHour: function(value, $el_1, $el_2) {
+		var val_1       = value.toString().charAt(0),
+			val_2       = value.toString().charAt(1),
+			fig_1_value = $el_1.find('.top').html(),
+			fig_2_value = $el_2.find('.top').html();
 
-      var that = this,
-        $top = $el.find('.top'),
-        $bottom = $el.find('.bottom'),
-        $back_top = $el.find('.top-back'),
-        $back_bottom = $el.find('.bottom-back');
+		if(value >= 10) {
+			// Animate only if the figure has changed
+			if(fig_1_value !== val_1) this.animateFigure($el_1, val_1);
+			if(fig_2_value !== val_2) this.animateFigure($el_2, val_2);
+		}
+		else {
+			// If we are under 10, replace first figure with 0
+			if(fig_1_value !== '0') this.animateFigure($el_1, 0);
+			if(fig_2_value !== val_1) this.animateFigure($el_2, val_1);
+		}
+	}
+};
 
-      // Before we begin, change the back value
-      $back_top.find('span').html(value);
-
-      // Also change the back bottom value
-      $back_bottom.find('span').html(value);
-
-      // Then animate
-      TweenMax.to($top, 0.8, {
-        rotationX: '-180deg',
-        transformPerspective: 300,
-        ease: Quart.easeOut,
-        onComplete: function() {
-
-          $top.html(value);
-
-          $bottom.html(value);
-
-          TweenMax.set($top, {
-            rotationX: 0
-          });
-        }
-      });
-
-      TweenMax.to($back_top, 0.8, {
-        rotationX: 0,
-        transformPerspective: 300,
-        ease: Quart.easeOut,
-        clearProps: 'all'
-      });
-    },
-
-    checkDay: function(value, $el_1, $el_2) {
-
-      var val_1 = value.toString().charAt(0),
-        val_2 = value.toString().charAt(1),
-        fig_1_value = $el_1.find('.top').html(),
-        fig_2_value = $el_2.find('.top').html();
-
-      if (value >= 10) {
-
-        // Animate only if the figure has changed
-        if (fig_1_value !== val_1) this.animateFigure($el_1, val_1);
-        if (fig_2_value !== val_2) this.animateFigure($el_2, val_2);
-      } else {
-
-        // If we are under 10, replace first figure with 0
-        if (fig_1_value !== '0') this.animateFigure($el_1, 0);
-        if (fig_2_value !== val_1) this.animateFigure($el_2, val_1);
-      }
-    }
-  };
-  // Let's go !
-  Countdown.init();
+// Let's go !
+Countdown.init();
