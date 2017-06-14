f98d3838a2f3c69ecda28ade23e1150373f008e7
Alexander Kuranda <akuranda@provectus.com>
2017-06-14 18:08:18 +0300

    complete task

--- development/js/views/store-item-template.js
+++ development/js/views/store-item-template.js
@@ -1,13 +1,17 @@
 $.templates({ 'store-item-template': ' \
-<article id="store-item-id{{:XML_ID}}" class="store-item  js-modal-window" data-xml-id="{{:XML_ID}}" data-js-modal-active-modifier="store-item--active"> \
+<article id="store-item-id{{:XML_ID}}" class="store-item  js-modal-window" data-xml-id="{{:XML_ID}}" data-js-modal-active-modifier="store-item--active"  itemscope itemtype=" http://schema.org/JewelryStore"> \
 	<div class="store-item__heading">\
-		<h3 class="store-item__title">585GOLD.<br>{{:CITY}}, {{:ADDRESS["STREET"]}} {{:ADDRESS["HOUSE_FLAT"]}}</h3> \
+		<h3 class="store-item__title">\
+			<span itemprop="name">585GOLD</span>.<br>\
+			<span itemprop="addressLocality">{{:CITY}}</span>, \
+			<span itemprop="streetAddress">{{:ADDRESS["STREET"]}} {{:ADDRESS["HOUSE_FLAT"]}}</span>\
+		</h3> \
 		<div id="store-item-close" class="store-item__close  js-modal-close-button">X</div>\
 	</div>\
 	<div class="store-item__group">\
 		<p class="store-item__group-text  store-item__group-text--schedule">\
 			Режим работы: <br>\
-			{{:SCHEDULE_PREPARE}}\
+			<time itemprop="openingHours" datetime="{{:SCHEDULE_PREPARE}}">{{:SCHEDULE_PREPARE}}</time>\
 		</p>\
 		<!-- Кнопка/ссылка на магазин -->\
 		<a href="{{:DETAIL_URL}}" class="b-button  store-item__button  store-item__button--schedule" data-store-url="{{:DETAIL_URL}}">Страница магазина</a>\
