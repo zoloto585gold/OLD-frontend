7ece79850304651d6f38d5ad1e2f6425ca2466c9
ma23 <23b.maa@gmail.com>
2017-11-30 14:41:40 +0300

    ESHOP-320: Проверка баланса на странице регистрации бонусной карты

--- development/js/app/z585.main.js
+++ development/js/app/z585.main.js
@@ -106,4 +106,24 @@
 
 	main.environmentDefine();
 
+	/**
+	 * Склонение существительного в зависимости от числительного
+	 * getPlural(var, 'магазин', 'магазина', 'магазинов')
+	 */
+	main.getPlural = function(number, one, two, five) {
+		var n = Math.abs(number);
+		n %= 100;
+		if (n >= 5 && n <= 20) {
+			return five;
+		}
+		n %= 10;
+		if (n === 1) {
+			return one;
+		}
+		if (n >= 2 && n <= 4) {
+			return two;
+		}
+		return five;
+	}
+
 } ());
