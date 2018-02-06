20aa94e0a8157f58e81cf473b11063c32a8e2ddb
dovgan.evgeniy <dovgan.evgeniy@zoloto585.ru>
2018-02-06 16:18:29 +0300

    ESHOP-668 | анимированный курсор на главной странице сайта

--- development/js/app/Z585.cursor.js
+++ development/js/app/Z585.cursor.js
@@ -5,47 +5,104 @@
 
 	var cursor = Z585.define('Z585.cursor');
 
-	// запуск функции после загрузки страницы
-	$(document).ready(function(e) {
-	
-		var a1 = document.getElementById("a1");
-		var cont = document.getElementsByTagName("html");
-		cont.onmousemove = moveEvent;
-		var timer;	
+	cursor.animateCursor = function() {
+
+		var zone = $('body');
 		var xPos;
 		var yPos;
 		var posObjx;
 		var posObjy;
-		
-		function moveEvent(){
-			var e;
-			if(!e){
-				e = window.event;
+		var scaleVal;
+		var mouseOver = false;
+
+		var mainmenu = $('.header-bottom');
+
+		mainmenu.hover(
+			function() {
+				mouseOver = true;
+				console.log(mouseOver);
+			},
+			function() {
+				mouseOver = false;
+				console.log(mouseOver);
 			}
-			
-			xPos = e.clientX;
-			yPos = e.clientY;
-			console.log('xPos: '+xPos+' yPos:'+yPos);
-			timer = setInterval(animateMove,150);
-		}
+		);
+
+		// период появленя элементов
+
+		var timer = setInterval(randomElements, 25);		
+
+		// узнаём положение мышки и сохраняем в переменную
+
+		zone.mousemove(function(event) {
+
+				xPos = event.pageX;
+				yPos = event.pageY;
+
+				if ( mouseOver == false ) {
+					
+					console.log('__');
+				} else {
+					clearInterval(timer);
+					console.log('наведены на меню');
+				}
+				
+		});
+
 		
-		function animateMove(){
-			posObjx = parseInt(a1.style.left);
-			posObjy = parseInt(a1.style.top);
-			a1.style.left = xPos + "px";
-			a1.style.top = yPos + "px";
-			clearInterval(timer);
+
+		function randomElements() {
+
+
+
+			if ((xPos - posObjx) == 0 && (yPos - posObjy) == 0) {
+
+				// Если мышь не двигается
+
+ 			} else {
+
+ 				// Если мышь в движении
+ 				
+ 				posObjx = xPos;
+      			posObjy = yPos;	
+      			scaleVal = 0;
+
+      			var element = $('<span class="heart"></span>');
+      			zone.append(element);
+				
+				// рандомное число от 1 до 10
+				var rand = Math.floor((Math.random() * 20) + 1);
+				
+				element.css({
+					'top': yPos - rand,
+					'left': xPos - rand
+				});
+
+				var p = setTimeout(function() {
+					element.addClass('heart-min');
+					clearTimeout(p);
+				},10);
+
+				var t = setTimeout(function() {
+					element.remove();
+					clearTimeout(t);
+				},1500);
+
+      			//console.log(posObjx+' : '+posObjy);
+ 			}
+
 		}
-			
-	
+
+		
+	}
+
+	// запуск функции после загрузки страницы
+	$(document).ready(function() {
+
+		cursor.animateCursor();
 
 	});
 
-	// функция генерирования кода окна
-	cursor.animateCursor = function() {
-		//alert('animate');
-	}
 
-	cursor.animateCursor();
 
 } ());
